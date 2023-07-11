51b262ccce1b07517719399d28a5c435d49cdcd2
Alteian <Alteian@proton.me>
2023-07-11 08:57:18 +0200

    FilterLookup test

--- core/user/graphql/types.py
+++ core/user/graphql/types.py
@@ -2,8 +2,16 @@ import typing
 import strawberry
 
 import strawberry_django
+from strawberry_django import field
 
 from core.user.models import User, RandomModel
+from strawberry_django.filters import FilterLookup
+@strawberry_django.filter(User, lookups=True)
+class UserFilter:
+    id: typing.Optional[FilterLookup[strawberry.relay.GlobalID]]
+    name: strawberry.auto
+    some_field: strawberry.auto
+
 
 @strawberry_django.type(RandomModel)
 class RandomModelType(strawberry.relay.Node):
@@ -11,10 +19,12 @@ class RandomModelType(strawberry.relay.Node):
     name: strawberry.auto
 
 
-@strawberry_django.type(User)
+@strawberry_django.type(User, filters=UserFilter)
 class UserType(strawberry.relay.Node):
-    id: strawberry.relay.NodeID[int]
+    #code: strawberry.relay.NodeID[int] = field(graphql_type=strawberry.relay.GlobalID, field_name="id")
     name: strawberry.auto
-    email: strawberry.auto
+    super_email: strawberry.auto = field(field_name="email")
     some_field: strawberry.auto
-    random_model: typing.Optional[RandomModelType]
+    #random_model: typing.Optional[int] = field(field_name="random_model_pk")
+    random_model_id: typing.Optional[int]
+    random_model: typing.Optional[int] = field(field_name="random_model_id")
